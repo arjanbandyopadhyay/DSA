bedba894e4baf207c8c7ea7e583cca6d00112d6c
arjanbandyopadhyay <arjanbandyopadhyay247@gmail.com>
2026-07-26 18:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.59%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+        return false;
+       unordered_map<char,int>mpp;
+       for(char c:s)
+       mpp[c]++;
+       for(char c:t)
+       mpp[c]--;
+
+       for(auto it:mpp){
+        if(it.second!=0)
+        return false;
+       }
+       return true;
+        
+    }
+};
